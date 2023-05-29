b20c77443cf16ec61550670c8d02581fa059967c
cueavyqwp <127200270+cueavyqwp@users.noreply.github.com>
2023-05-29 11:04:45 +0800

    add new feature , bug fix

--- EdgeGPT-GUI.py
+++ EdgeGPT-GUI.py
@@ -10,13 +10,15 @@ import time
 import pip
 import os
 
-#==================================#
-font = ( "Consolas" , 18 , "bold" )
-cookies = "cookies.json"
-chat_logs = "chat_logs"
-logs = "logs"
-save_logs_file = True
-#==================================#
+#===================================#
+font = ( "Consolas" , 18 , "bold" ) # 自定义字体
+cookies = "cookies.json"            # cookies文件位置 
+chat_logs = "chat_logs"             # 聊天记录位置
+logs = "logs"                       # 日志文件位置
+save_logs_file = True               # 是否保存日志
+show_links = True                   # 是否在GUI上显示链接
+show_suggest = True                 # 是否在GUI上显示建议
+#===================================#
 
 file_name = os.path.join( str( time.strftime( "%Y-%m-%d" , time.localtime() ) ) )
 logs_name = os.path.join( logs , file_name + ".log" )
@@ -90,10 +92,13 @@ def md_text( key , value ) :
     i = md_texts[ key ].split( "<!--value-->" )
     return i[0] + value + i[1] 
 
+def md_file() :
+    return open( chat_logs_name , "a" , encoding = "utf-8" )
+
 def log_time( type ) :
-    with open( chat_logs_name , "a" , encoding = "utf-8" ) as File :
+    with md_file() as file :
         Now_time = time.strftime( "%Y-%m-%d %H:%M:%S" , time.localtime() )
-        File.write( md_text( type , Now_time ) )
+        file.write( md_text( type , Now_time ) )
         logger.info( f"Log time [{type}] [ {Now_time} ]" )
 
 def reset( *args ) :
@@ -114,33 +119,59 @@ def reset( *args ) :
     else :
         tkmsg.showinfo( lang.get( "info" ) , lang.get( "wait" ) )
 
-def show_count( *args ): #统计字数
+def show_count( *args ) : # 统计字数
     global the_text
     the_text = text.get( "1.0" , "end" ) [:-1]
     root.title( lang.str_replace ( f"EdgeGPT-GUI [ { lang.get( 'word' ) } 2000/{ len( the_text ) } ] " + lang.replace( "help" , [ "[ " , " ] [ " , " ]" ] ) ) )
     root.after( 1 , show_count )
 
-def add_chat_message( message , enter = True ): #往聊天内容里添加内容
+def add_chat_message( message = "" , enter = True ) : # 往聊天内容里添加内容
     chat_text.config( state = tk.NORMAL )
     chat_text.insert( tk.END , message + ( "" , "\n" ) [enter] )
     chat_text.config( state = tk.DISABLED )
     chat_text.see( tk.END )
 
-def Bing_s_message( future ):
+def Bing_s_message( future ) :
     global text , can_chat , the_text_old
     try :
         data = future.result()
-        message = data["item"]["messages"][1]["text"]
-        suggested_responses = data["item"]["messages"][1]["suggestedResponses"]
-        with open( chat_logs_name , "a" , encoding = "utf-8" ) as File :
-            File.write( md_text( "bing" , message ) )
-        add_chat_message( f"{message}\n" )
+        if not ( len( data["item"]["messages"] ) -1 ) :
+            tkmsg.showerror( lang.get( "wrong" ) , lang.get( "can_not_get_message" ) )
+        messages = data["item"]["messages"][1]
+        message = messages["text"]
+        message = message.replace( "[^" , "[" ).replace( "^]" , "]" )
+        links = messages["sourceAttributions"]
         logger.info("Finish to get message")
-        if len(suggested_responses) :
-            logger.info( "-" * 30 )
+        if len( links ) :
+            link_text = ""
+            for i in range( len( links ) ) :
+                if links[i] :
+                    link_text += f"""[{i+1}] {links[i]["seeMoreUrl"]}\n"""
+                    with md_file() as file :
+                        file.write( f"""[{i+1}]: {links[i]["seeMoreUrl"]}\n""" )
+            link_text = link_text[:-1]
+            if show_links :
+                link_text = f"""{"-" * 30}\n{link_text}\n{"-" * 30}"""
+                add_chat_message( link_text )
+        elif show_links :
+            add_chat_message()
+        suggested_responses = data["item"]["messages"][1]["suggestedResponses"]
+        with md_file() as file :
+            file.write( md_text( "bing" , message ) )
+        if ( not show_suggest ) or ( not len( suggested_responses ) ) :
+            message += "\n"
+        add_chat_message( message )
+        if len( suggested_responses ) :
+            suggest_text = ""
             for i in suggested_responses :
-                logger.info(i[ "text" ])
+                if i :
+                    suggest_text += i[ "text" ] + "\n"
+            suggest_text = suggest_text[:-1]
+            logger.info( "-" * 30 + "\n" + suggest_text )
             logger.info( "-" * 30 )
+            if show_suggest :
+                suggest_text = f"""{"-" * 30}\n{suggest_text}\n{"-" * 30}"""
+                add_chat_message( suggest_text )
         message_user()
     except :
         logger.error("Fail to get message")
@@ -152,24 +183,23 @@ def Bing_s_message( future ):
         text.insert( tk.END , the_text_old )
     can_chat = True
 
-def send( *args ):
+def send( *args ) :
     global the_text_old , can_chat
-
     if not can_chat :
         tkmsg.showinfo( lang.get( "info" ) , lang.get( "wait" ) )
-
     elif not the_text or "".join( the_text.split() ) == "" :
         tkmsg.showerror( lang.get( "wrong" ) , lang.get( "empty_content" ) )
-
     elif len( the_text ) > 2000 :
         tkmsg.showinfo( lang.get( "info" ) , lang.get( "words_too_more" ) )
-
     elif len( the_text ) <= 2000 and can_chat :
         can_chat = False
-        with open( chat_logs_name , "a" , encoding = "utf-8" ) as File :
-            File.write( md_text( "user" , the_text ) )
+        with md_file() as file :
+            file.write( md_text( "user" , the_text ) )
         add_chat_message( f"{the_text}" )
-        add_chat_message( "\nBing:\n" )
+        if show_links :
+            add_chat_message( "\nBing:" )
+        else :
+            add_chat_message( "\nBing:\n" )
         the_text_old = the_text
         text.delete( 0.0 , "end" )
         future = EdgeGPT.asyncio.run_coroutine_threadsafe( ask() , loop )
@@ -223,8 +253,8 @@ logger.info( "-" * 30 )
 logger.info( "'EdgeGPT-GUI' run" )
 logger.debug( f"'font' info [ {font} ]" )
 logger.debug( f"'cookies' file at '{os.path.abspath( cookies )}'" )
-logger.debug( f"'chat_logs' file at '{os.path.abspath( chat_logs )}'" )
-logger.debug( f"'logs' file at '{os.path.abspath( logs )}'" )
+logger.debug( f"'chat_logs' dir at '{os.path.abspath( chat_logs )}'" )
+logger.debug( f"'logs' dir at '{os.path.abspath( logs )}'" )
 
 log_time( "start" )
 message_user()
